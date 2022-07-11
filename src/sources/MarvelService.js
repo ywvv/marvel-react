dfb08ff058bd7dd7113835dadafe40c4209b12dc
Vlad Horban <ywvvvw@gmail.com>
2022-07-11 15:28:19 +0300

    feat: add random char

--- src/sources/MarvelService.js
+++ src/sources/MarvelService.js
@@ -12,12 +12,24 @@ class MarvelService {
     return await res.json()
   }
 
-  getAllChar = () => {
-    return this.getRes(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+  getAllChar = async () => {
+    const res = await this.getRes(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    return res.data.results.map(this._transformChar)
   }
 
-  getChar = (id) => {
-    return this.getRes(`${this._apiBase}characters/${id}?${this._apiKey}`)
+  getChar = async (id) => {
+    const res = await this.getRes(`${this._apiBase}characters/${id}?${this._apiKey}`)
+    return this._transformChar(res.data.results[0])
+  }
+
+  _transformChar = (char) => {
+    return {
+      name: char.name,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
   }
 }
 
